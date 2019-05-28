095285a2fe454418ddff798e664b8f153ecb6d11
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-05-28 15:37:57 -0400

    solved task 11

--- src/11_args.py
+++ src/11_args.py
@@ -6,12 +6,29 @@
 
 # YOUR CODE HERE
 
+
+def f1(a, b):
+    return a+b
+
+
 print(f1(1, 2))
 
 # Write a function f2 that takes any number of integer arguments and prints the
 # sum. Google for "python arbitrary arguments" and look for "*args"
 
 # YOUR CODE HERE
+
+
+def f2(*args):
+    result = 0
+    for arg in args:
+        if type(arg) in (int, float):  # or to make a list of all the numbers and use sum() on it
+            result += arg
+        elif type(arg) in (list, tuple):
+            for elem in arg:
+                result += elem
+    return result
+
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
@@ -29,6 +46,11 @@ print(f2(a))    # Should print 22
 
 # YOUR CODE HERE
 
+
+def f3(a, b=1):
+    return a+b
+
+
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
 
@@ -42,6 +64,20 @@ print(f3(8))     # Should print 9
 # Google "python keyword arguments".
 
 # YOUR CODE HERE
+def f4(*args, **kwargs):
+    for arg in args:
+        if type(arg) == dict:
+            for k, v in arg.items():
+                print(k, v)
+        else:
+            print(arg)
+    for key, value in kwargs.items():
+        if type(value) == dict:
+            for k, v in value.items():
+                print(k, v)
+        else:
+            print(key, value)
+
 
 # Should print
 # key: a, value: 12
