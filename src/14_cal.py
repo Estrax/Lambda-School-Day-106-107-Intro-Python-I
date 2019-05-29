beac3458f3eb2cc12f61a0cab9e53ba0de04dfe7
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-05-29 13:28:46 -0400

    fixed tasks a little bit

--- src/14_cal.py
+++ src/14_cal.py
@@ -4,19 +4,19 @@ render a calendar to your terminal.
 https://docs.python.org/3.6/library/calendar.html
 
 Write a program that accepts user input of the form
-  `calendar.py month [year]`
+	`calendar.py month [year]`
 and does the following:
  - If the user doesn't specify any input, your program should
-   print the calendar for the current month. The 'datetime'
-   module may be helpful for this.
+	 print the calendar for the current month. The 'datetime'
+	 module may be helpful for this.
  - If the user specifies one argument, assume they passed in a
-   month and render the calendar for that month of the current year.
+	 month and render the calendar for that month of the current year.
  - If the user specifies two arguments, assume they passed in
-   both the month and the year. Render the calendar for that
-   month and year.
+	 both the month and the year. Render the calendar for that
+	 month and year.
  - Otherwise, print a usage statement to the terminal indicating
-   the format that your program expects arguments to be given.
-   Then exit the program.
+	 the format that your program expects arguments to be given.
+	 Then exit the program.
 """
 
 import sys
@@ -29,21 +29,21 @@ cal = calendar.TextCalendar(calendar.SUNDAY)
 def create_calendar(calendar_input):
     try:
         now = datetime.now()
-        input_date = calendar_input.split()
-        if(not input_date or len(input_date) not in range(3)):
+        if (len(calendar_input) not in range(3)):
             print("Please type the date in the following format: MM YYYY")
             return
-
-        cases = {
-            0: cal.formatmonth(now.year, now.month),
-            1: cal.formatmonth(now.year, int(input_date[0])),
-            2: cal.formatmonth(int(input_date[1]), int(input_date[0]))
-        }
-        print(cases[len(input_date)])
+        if len(calendar_input) == 0:
+            print(cal.formatmonth(now.year, now.month))
+        elif len(calendar_input) == 1:
+            print(cal.formatmonth(now.year, int(calendar_input[0])))
+        elif len(calendar_input) == 2:
+            print(cal.formatmonth(
+                int(calendar_input[1]), int(calendar_input[0])))
     except Exception as e:
         print(f'Error: {e}')
         print("Please type the date in the following format: MM YYYY")
+        exit()
 
 
-calendar_input = input('Please input month and year (format: MM YYYY):')
+calendar_input = sys.argv[1:]
 create_calendar(calendar_input)
